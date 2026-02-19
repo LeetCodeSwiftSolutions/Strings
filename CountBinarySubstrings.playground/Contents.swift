7af9d2fe6caaf65c12d8b323722ea0c293319ca0
Miguel Torres <99153283+MrPicudo@users.noreply.github.com>
2026-02-18 20:56:29 -0600

    Count binary substrings [easy]
    - Convertimos una cadena en un arreglo de caracteres para facilitar su manejo en ciclos.
    - Usamos el método .min() para obtener el mínimo de dos números
    - Usamos un contador "counter" para saber el número de veces que se ha cambiado de caracter en la subcadena.

---
+++ CountBinarySubstrings.playground/Contents.swift
@@ -0,0 +1,48 @@
+/*  696. Count binary substrings [easy]
+ Given a binary string s, return the number of non-empty substrings that have the same number of 0's and 1's, and all the 0's and all the 1's in these substrings are grouped consecutively. Substrings that occur multiple times are counted the number of times they occur.
+ 
+ Dada una cadena binaria s, devuelve el número de subcadenas no vacías que tengan la misma cantidad de 0's y 1's, y donde todos los 0's y todos los 1's dentro de esas subcadenas estén agrupados consecutivamente. Las subcadenas que aparezcan varias veces se cuentan tantas veces como ocurran. */
+
+///
+/// Original Solution.
+/// Time Complexity: O(n), Space Complexity O(n).
+/// Beats 29% in Runtime & 36% in Memory.
+///
+func countBinarySubstrings(_ s: String) -> Int {
+    var zeros = 0
+    var ones = 0
+    var arr = Array(s)
+    var counter = 1
+    var ans = 0
+
+    for i in 0..<arr.count {
+        if i > 0 && arr[i-1] != arr[i] { counter += 1 }
+        switch arr[i] {
+            case Character("0"):
+            zeros += 1
+            if counter > 2 {
+                ans += min(zeros-1, ones)
+                zeros = 1
+                counter = 2
+            }
+            default:
+            ones += 1
+            if counter > 2 {
+                ans += min(zeros, ones-1)
+                ones = 1
+                counter = 2
+            }
+        }
+    }
+    
+    ans += min(zeros, ones)
+    return ans
+}
+
+
+///
+/// Original test cases
+///
+countBinarySubstrings("0000100011111") // Expected: 5
+countBinarySubstrings("0101010100") // Expected: 8
+countBinarySubstrings("1111111110") // Expected: 1
